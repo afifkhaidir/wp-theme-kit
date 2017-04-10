182ae996bef5a97efd1df4246b305d24ee4ff422
Khaidir Afif <afif.khaidir@gmail.com>
2017-04-10 14:02:25 +0000

    Improve index page design, customize components

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php get_header(); ?>
   <div class="max-width-4 mx-auto p2">
-    <div class="post">
+    <div class="post my3">
       <a href="#" class="post-image block">
-        <img src="https://i0.wp.com/static.pexels.com/photos/292600/pexels-photo-292600.jpeg" alt="" class="post-image__img fit">
+        <img src="https://i0.wp.com/static.pexels.com/photos/292600/pexels-photo-292600.jpeg?resize=990,400" alt="" class="post-image__img fit">
       </a>
-      <h1 class="h2 mt3 mb1">Salak Mountain</h1>
+      <h1 class="h2 mt3 mb1 bold">Salak Mountain</h1>
       <div class="post-detail mb2">
         <div class="inline-block">
           <a href="#" class="post-detail__category mr1">Front End</a>
           <a href="#" class="post-detail__category mr1">Ecmascript 6</a>
         </div>
       </div>
-      <p class="post__p">
+      <p class="post__p mt2">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
         sed diam nonummy nibh euismod tincidunt ut laoreet dolore
         magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
@@ -21,6 +21,30 @@
         vel illum dolore eu feugiat nulla facilisis at vero eros et
         accumsan et iusto odio dignissim qui blandit praesent luptatum
       </p>
+      <a href="#" class="btn btn-primary mt2 mb3">Selengkapnya</a>
+    </div>
+    <div class="post">
+      <a href="#" class="post-image block">
+        <img src="https://i0.wp.com/static.pexels.com/photos/38892/pexels-photo-38892.jpeg?resize=990,400" alt="" class="post-image__img fit">
+      </a>
+      <h1 class="h2 mt3 mb1 bold">Bring Your Laptop</h1>
+      <div class="post-detail mb2">
+        <div class="inline-block">
+          <a href="#" class="post-detail__category mr1">Front End</a>
+          <a href="#" class="post-detail__category mr1">Ecmascript 6</a>
+        </div>
+      </div>
+      <p class="post__p mt2">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+        sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+        magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+        quis nostrud exerci tation ullamcorper suscipit lobortis nis
+        lut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+        dolor in hendrerit in vulputate velit esse molestie consequat,
+        vel illum dolore eu feugiat nulla facilisis at vero eros et
+        accumsan et iusto odio dignissim qui blandit praesent luptatum
+      </p>
+      <a href="#" class="btn btn-primary my2">Selengkapnya</a>
     </div>
   </div>
 <?php get_footer(); ?>
